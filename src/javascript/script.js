6aea7f61f649e5716f21055497e9fc662944cb7f
RafaelCristino79 <rafaelcristino79@gmail.com>
2024-07-22 17:11:25 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,18 @@
+const btnmobile = document.querySelector(".btn-mobile");
+const menumobile = document.querySelector(".nav_list");
+
+btnmobile.addEventListener("click", () => {
+    btnmobile.classList.toggle('active');    
+    menumobile.classList.toggle('active');    
+})
+
+
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(accordion => (
+    accordion.addEventListener('click', () =>{
+        const body = accordion.querySelector('.accordion-body');
+        body.classList.toggle('active');
+    })
+))

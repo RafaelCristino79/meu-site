a4ea122fe9338af6516572743c7966d98e5bd645
RafaelCristino79 <rafaelcristino79@gmail.com>
2024-07-24 15:26:54 -0300

    Att

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -16,3 +16,12 @@ accordions.forEach(accordion => (
         body.classList.toggle('active');
     })
 ))
+
+
+function redirectToExternalDownload() {
+    // Link para o arquivo externo
+    var externalDownloadLink = "https://drive.usercontent.google.com/download?id=1Nr1ga8pGj3Y4IuRxmCMx0EoKpqVu0CRr&export=download";
+    
+    // Redireciona o navegador para o link de download externo
+    window.location.href = externalDownloadLink;
+  }

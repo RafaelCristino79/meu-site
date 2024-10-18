0eba32e2af2984a50fd19290fe3abc64a9b1e7c0
RafaelCristino79 <rafaelcristino79@gmail.com>
2024-10-17 23:17:17 -0300

    slider

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -98,3 +98,34 @@ accordions.forEach(accordion => {
   });
 });
 
+new Swiper('.card-wrapper', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 10,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  
+  });
